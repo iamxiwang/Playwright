b946155f04f5da308b430a65fd095e7ef6d6bf3f
XiWang <vivian.xi.wang@gmail.com>
2023-05-24 19:48:40 -0700

    tested netflix sign in with invalid credential

--- tests/my-first-test.spec.js
+++ tests/my-first-test.spec.js
@@ -1,8 +1,144 @@
-const {test, expect} = require('@playwright/test')
+const { test, expect } = require("@playwright/test");
 
+// run tests in headful mode so you can see the browser
+test.use({ headless: false, slowMo: 1000 });
 
-test('Myfirst test', async ({page}) => {
-    await page.goto('https://greenfin.onrender.com/')
-    await expect(page).toHaveTitle('GreenFin')
+test("my first test", async ({ page }) => {
+  // go to Netflix.com
+    await page.goto("https://www.netflix.com");
 
-})
+    await expect(
+        page.locator('[data-uia="nmhp-card-hero-text-title"]')
+    ).toHaveText("Unlimited movies, TV shows, and more.");
+});
+
+// ADD YOUR TESTS HERE!
+
+test.beforeEach(async ({ page }) => {
+  // go to Netflix
+    await page.goto("https://www.netflix.com");
+});
+
+// Test 1: Sign In with Invalid Email
+
+test("Test 1: Sign In with Invalid Email ", async ({ page }) => {
+  // find Sign In locator and click it
+    await page.locator("[data-uia='header-login-link']").click();
+
+    // url should be changed to https://www.netflix.com/login
+    await expect(page).toHaveURL(/.*login/);
+
+    // fill out email field
+    await page.fill("#id_userLoginId", "test-email@gmail.com");
+
+    // fill out password field
+    await page.fill("#id_password", "test-password123");
+
+    // check if there is Sign In button
+    await expect(page.locator('[data-uia="login-page-title"]')).toHaveText(
+        "Sign In"
+    );
+
+    // click Enter keystroke to submit form
+    await page.keyboard.press("Enter");
+
+    // expect to see error message
+    await expect(page.locator('[data-uia="text"]')).toContainText(
+        "Sorry, we can't find an account with this email address. Please try again"
+    );
+
+    await page.waitForTimeout(1000);
+});
+
+// Test 2: Sign In with Incorrect Password
+
+test("Test 2: Sign In with Incorrect Password", async ({ page }) => {
+  // find Sign In locator and click it
+    await page.locator("[data-uia='header-login-link']").click();
+
+    // url should be changed to https://www.netflix.com/login
+    await expect(page).toHaveURL(/.*login/);
+
+    // fill out email field
+    await page.fill("#id_userLoginId", "youremail@gmail.com");
+
+    // fill out password field
+    await page.fill("#id_password", "blablabla");
+
+    // check if there is Sign In button
+    await expect(page.locator('[data-uia="login-page-title"]')).toHaveText(
+        "Sign In"
+    );
+
+  // find Sign In locator and click it
+    await page.locator("[data-uia='login-submit-button']").click();
+
+    // expect to see error message
+    await expect(page.locator('[data-uia="text"]')).toContainText(
+        "Incorrect password."
+    );
+
+    await page.waitForTimeout(1000);
+});
+
+// Test 3: Sign In with Short Password
+
+test("Test 3: Sign In with Short Password", async ({ page }) => {
+  // find Sign In locator and click it
+    await page.locator("[data-uia='header-login-link']").click();
+
+    // url should be changed to https://www.netflix.com/login
+    await expect(page).toHaveURL(/.*login/);
+
+    // fill out email field
+    await page.fill("#id_userLoginId", "test-email@gmail.com");
+
+    // fill out password field
+    await page.fill("#id_password", "123");
+
+    // check if there is Sign In button
+    await expect(page.locator('[data-uia="login-page-title"]')).toHaveText(
+        "Sign In"
+    );
+
+  // find Sign In locator and click it
+    await page.locator("[data-uia='login-submit-button']").click();
+
+  // expect to see error message
+    await expect(page.locator('[data-uia="password-field+error"]')).toContainText(
+        "Your password must contain between 4 and 60 characters."
+    );
+
+    await page.waitForTimeout(1000);
+});
+
+// Test 4: Sign In with Invalid Phone Number
+
+test("Test 4: Sign In with Invalid Phone Number", async ({ page }) => {
+  // find Sign In locator and click it
+    await page.locator("[data-uia='header-login-link']").click();
+
+    // url should be changed to https://www.netflix.com/login
+    await expect(page).toHaveURL(/.*login/);
+
+    // fill out email field
+    await page.fill("#id_userLoginId", "123456789");
+
+    // fill out password field
+    await page.fill("#id_password", "test-password123");
+
+    // check if there is Sign In button
+    await expect(page.locator('[data-uia="login-page-title"]')).toHaveText(
+        "Sign In"
+    );
+
+    // find Sign In locator and click it
+    await page.locator("[data-uia='login-submit-button']").click();
+
+    // expect to see error message
+    await expect(page.locator('[data-uia="text"]')).toContainText(
+        "Sorry, we can't find an account with this number."
+    );
+
+    await page.waitForTimeout(1000);
+});
---
+++ tests/netflix.spec.js
@@ -0,0 +1,151 @@
+const { test, expect } = require("@playwright/test");
+
+// run tests in headful mode so you can see the browser
+test.use({ headless: false, slowMo: 1000 });
+test.beforeEach(async({page}) => {
+  await page.goto('http://www.netflix.com');
+})
+
+
+test('T1: User cannot sign in with invaild email/password combination on Netflix', async( {page}) => {
+
+      //Click Sign in button
+      await page.click('text= Sign In')
+
+      expect(page.url()).toContain('/login')
+
+     // check if in the Sign in form existing
+     //check if the form existing
+      const emailField = page.locator('.nfInput.nfEmailPhoneInput.login-input.login-input-email')
+      const passwordField = page.locator('[data-uia="password-field+container"]')
+      const signInButton = page.locator('button[class="btn login-button btn-submit btn-small"]')
+
+
+      await emailField.waitFor()
+      await passwordField.waitFor()
+      await signInButton.waitFor()
+
+      expect (emailField).toBeVisible()
+      expect(passwordField).toBeVisible()
+      expect(signInButton).toBeVisible()
+
+      // Input invaild email and password
+      await page.fill('input[data-uia="login-field"]','invalidemail@invalid.com' )
+      await page.fill('input[data-uia="password-field"]','invalidpassword' )
+
+      //click the submit button
+      await page.click('button[data-uia="login-submit-button"]')
+
+      // locate the error message
+      const errorMessageElement = page.locator('[class="ui-message-contents"]')
+      //get errormessage text
+      const errorMessage = await errorMessageElement.textContent()
+      expect(errorMessage).toContain("Sorry, we can't find an account with this email address. Please try again or create a new account.")
+  
+})
+
+test('T2: Edge cases: empty email and empty password  show error messages', async ({ page }) => {
+    
+    //Click Sign in button
+    await page.click('text= Sign In')
+
+    //check if the form existing
+    const emailField = page.locator('.nfInput.nfEmailPhoneInput.login-input.login-input-email')
+    const passwordField = page.locator('[data-uia="password-field+container"]')
+    const signInButton = page.locator('button[class="btn login-button btn-submit btn-small"]')
+
+
+    await emailField.waitFor()
+    await passwordField.waitFor()
+    await signInButton.waitFor()
+
+    expect (emailField).toBeVisible()
+    expect(passwordField).toBeVisible()
+    expect(signInButton).toBeVisible()
+
+    // Input invaild email and password
+    await page.fill('input[data-uia="login-field"]','' )
+    await page.fill('input[data-uia="password-field"]','' )
+    // Click sign-in button without filling in the fields
+    await page.click('.login-button');
+
+
+    // Locate the error message
+    const emailErrorMessage = page.locator('[data-uia="login-field+error"]');
+    const passwordErrorMessage = page.locator('[data-uia="password-field+error"]');
+
+    // Assert that the error messages are what we expect
+    expect(await emailErrorMessage.textContent()).toBe("Please enter a valid email or phone number.");
+    expect(await passwordErrorMessage.textContent()).toBe("Your password must contain between 4 and 60 characters.");
+
+    await page.waitForTimeout(1000)
+});
+
+test('T3: Using invalid email format to sign in', async({page}) => {
+    page.click('text=sign in')
+
+    const emailField = page.locator('[data-uia = "login-field+container"]')
+    const passwordField = page.locator('.nfInput.nfPasswordInput.login-input.login-input-password')
+    const signInButton = page.locator('.btn.login-button.btn-submit.btn-small')
+
+    await emailField.waitFor()
+    await passwordField.waitFor()
+    await signInButton.waitFor()
+
+    expect (emailField).toBeVisible()
+    expect(passwordField).toBeVisible()
+    expect(signInButton).toBeVisible()
+
+    // Input invaild email and password
+    await page.fill('input[id="id_userLoginId"]','notanemailaddress' )
+    await page.fill('input[name="password"]','123456' )
+    // Click sign-in button without filling in the fields
+    await page.click('.login-button');
+
+    const errorMessage = page.locator('.ui-message-contents')
+    expect(await errorMessage.textContent()).toContain("Sorry, we can't find an account with this email address. Please try again")
+
+    await page.waitForTimeout(1000)
+})
+
+
+test('T4: Press enter key  with invalid password', async ({page}) =>{
+      await page.click('text =sign in')
+
+      await page.fill('input[name="userLoginId"]', 'test@email.com')
+      await page.fill('input[type="password"]', '123')
+      //Simulate pressing the Enter key
+      await page.press('input[id="id_password"]', 'Enter')
+
+      const errorMessage = page.locator('.ui-message-contents')
+      expect( await errorMessage.textContent()).toContain('Incorrect password. Please try again or you can reset your password.')
+      await page.waitForTimeout(1000)
+})
+
+test('T5: Click sign in button with invalid password', async ({page}) =>{
+  await page.click('text =sign in')
+
+  await page.fill('input[name="userLoginId"]', 'test@email.com')
+  await page.fill('input[type="password"]', '123')
+
+
+  //Simulate click the sign in button
+  await page.click('.btn.login-button.btn-submit.btn-small')
+
+  const errorMessage = page.locator('[data-uia="password-field+error"]')
+
+  expect( await errorMessage.textContent()).toContain('Your password must contain between 4 and 60 characters.')
+})
+
+
+test('T6: login with valid email address but invalid password', async ({page}) => {
+    await page.click('text=sign in')
+
+    await page.fill('input[name="userLoginId"]', 'test@email.com')
+    await page.fill('input[type="password"]', 'password')
+    await page.click('button:has-text("sign in")')
+
+    const errorMessage = page.locator('.ui-message-contents')
+
+    expect(await errorMessage.textContent()).toContain('Incorrect password. Please try again or you can reset your password.')
+})
